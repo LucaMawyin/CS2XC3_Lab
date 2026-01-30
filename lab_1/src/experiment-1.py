a36856b75e98c46b41eef7cd51eb986eccabdd37
Anderson Ray <goandersonray@gmail.com>
2026-01-29 20:12:07 -0500

    added exp-1 report, decided not to use log scale on y axis as parabolic curve is easy to see. and removed good_sorts from exp-1 as it wasn't asked for in instructions

--- lab_1/src/experiment-1.py
+++ lab_1/src/experiment-1.py
@@ -1,5 +1,4 @@
 import bad_sorts
-import good_sorts
 
 import time
 import matplotlib.pyplot as plt
@@ -9,14 +8,12 @@ def run():
     algorithms = [
         (bad_sorts.insertion_sort, "Insertion Sort"),
         (bad_sorts.bubble_sort, "Bubble Sort"),
-        (bad_sorts.selection_sort, "Selection Sort"),
-        (good_sorts.quicksort, "Quick Sort"),
-        (good_sorts.mergesort, "Merge Sort"),
-        (good_sorts.heapsort, "Heap Sort")
+        (bad_sorts.selection_sort, "Selection Sort")
     ]
     
     rng = range(0, 1000,10) 
-    
+    time_factor = 1000 # seconds -> milliseconds
+
     plt.figure(figsize=(10, 6))
 
     for sort_func, label in algorithms:
@@ -25,9 +22,9 @@ def run():
         for i in rng:
             L = bad_sorts.create_random_list(i, 100000)
             
-            start = time.perf_counter()
+            start = time.perf_counter() * time_factor
             sort_func(L)
-            end = time.perf_counter()
+            end = time.perf_counter() * time_factor
             
             runtimes.append(end - start)
         
@@ -35,9 +32,9 @@ def run():
         
 
     plt.title("Sorting Algorithm Performance Comparison")
-    plt.yscale('log')
+    #plt.yscale('log')
     plt.xlabel("List Length (n)")
-    plt.ylabel("Time (seconds)")
+    plt.ylabel("Time (milliseconds)")
     plt.legend()
     plt.grid(True)
     plt.show()
