cf895b33fb50ebddfab90ea53a0c96f03afe8265
Anderson Ray <goandersonray@gmail.com>
2026-04-01 16:57:41 -0400

    fixed some renaming of min heap element class for A star

--- final_lab/src/final_project_part2.py
+++ final_lab/src/final_project_part2.py
@@ -2,21 +2,22 @@ import min_heap
 from final_project_part1 import *
 
 
-class AStarElement(min_heap.Element):
-    def __init__(self, value, key, h_val):
-        super().__init__(value, key)
-        self.h_val = h_val
+class AStarElement():
+    def __init__(self, value, g_score, h_score):
+        self.value = value
+        self.g_score = g_score
+        self.h_score = h_score
 
     @property
     def key(self):
-        return self.h_val + self._key
+        return self.g_score + self.h_score  # current weight + estimated weight left (which is a lower bound)
 
     @key.setter
     def key(self, value):
-        self._key = value
+        self.g_score = value
 
     def __str__(self):
-        return f"({self.value}, {self.h_val}, {self._key} | {self.key})"
+        return f"({self.value}, {self.g_score}, {self.h_score} | {self.key})"
 
 
 def a_star(G: DirectedWeightedGraph, s, d, h):
@@ -35,19 +36,17 @@ def a_star(G: DirectedWeightedGraph, s, d, h):
     while not Q.is_empty():
         current_element = Q.extract_min()
         current_node = current_element.value
-        dist[current_node] = current_element._key
+        dist[current_node] = current_element.g_score
 
         if current_node == d:
             return pred, dist[current_node]
 
         for neighbour in G.adj[current_node]:
-            if neighbour not in Q.map: continue
+            if neighbour not in Q.map:
+                continue
             if dist[current_node] + G.w(current_node, neighbour) < dist[neighbour]:
                 Q.decrease_key(neighbour, dist[current_node] + G.w(current_node, neighbour))
                 dist[neighbour] = dist[current_node] + G.w(current_node, neighbour)
                 pred[neighbour] = current_node
 
     return pred, float("inf")
-
-
-
