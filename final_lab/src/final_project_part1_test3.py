29e9ac8cf5ddb359beefc83a938883b5e817d86c
lucamawyin <lucamawyin@gmail.com>
2026-04-04 17:03:16 -0400

    p1 e1-3 complete

--- final_lab/src/final_project_part1_test3.py
+++ final_lab/src/final_project_part1_test3.py
@@ -18,18 +18,18 @@ def create_random_sparse_graph(n, upper, p):
 
 # Testing dijkstra_approx and bellman_ford_approx for edges
 
-number_of_nodes = 500
 source = 0
 
-p_values = [(i*0.01) for i in range(1,11)]
+p=0.05
+n_values = [i for i in range(100,500,50)]
 k_values = [1,3,5]
 
 d_scores = {k: [] for k in k_values}
 b_scores = {k: [] for k in k_values}
 
 for k in k_values:
-    for p in p_values:
-        G = create_random_sparse_graph(number_of_nodes, 10, p)
+    for n in n_values:
+        G = create_random_sparse_graph(n, 100, p)
         
         true_dijkstra = total_dist(dijkstra(G, source))
         true_bellman_ford = total_dist(bellman_ford(G, source))
@@ -37,19 +37,19 @@ for k in k_values:
         approx_dijkstra = total_dist(dijkstra_approx(G, source, k))
         approx_bellman_ford = total_dist(bellman_ford_approx(G, source, k))
 
-        d_score = abs(true_dijkstra - approx_dijkstra)
-        b_score = abs(true_bellman_ford - approx_bellman_ford)
+        d_score = abs(true_dijkstra - approx_dijkstra) / n
+        b_score = abs(true_bellman_ford - approx_bellman_ford) / n
 
         d_scores[k].append(d_score)
         b_scores[k].append(b_score)
 
 
 for k in k_values:
-    plt.plot(p_values, d_scores[k], label=f'Dijkstra Approx (k={k})', marker='o')
-    plt.plot(p_values, b_scores[k], label=f'Bellman-Ford Approx (k={k})', marker='s')
+    plt.plot(n_values, d_scores[k], label=f'Dijkstra Approx (k={k})', marker='o')
+    plt.plot(n_values, b_scores[k], label=f'Bellman-Ford Approx (k={k})', marker='s')
 
-plt.xlabel('Graph Density (p)')
-plt.ylabel('Total Distance Error (approx)')
-plt.title('Total Distance Error vs Graph Density (Sparse Graph)')
-plt.legend()
+plt.xlabel('Graph Size (Number of Nodes)')
+plt.ylabel('Relative Distance Error (Approx)')
+plt.title('Relative Distance Error vs Graph Size (Sparse Graph)')
+plt.legend(loc="center left")
 plt.show()
